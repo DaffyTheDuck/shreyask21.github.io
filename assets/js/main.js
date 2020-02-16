3f4b0d028878085f324b73e759bddf26ba076d7d
Shreyas Kulkarni <shreyaskulkarni202@gmail.com>
2020-02-16 22:47:47 +0530

    Fix typo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Strata by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var $window = $(window),
@@ -12,11 +6,7 @@
 		$footer = $('#footer'),
 		$main = $('#main'),
 		settings = {
-
-			// Parallax background effect?
 				parallax: true,
-
-			// Parallax factor (lower = more intense, higher = less intense).
 				parallaxFactor: 20
 
 		};
